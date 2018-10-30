ed90614ec4506ba044742757ba0474a3f9300df2
sergey <sergey_the_great@mail.ru>
2018-10-30 17:15:38 +0500

    Сделал до страницы 170.

---
+++ app/src/main/java/com/example/zes/criminalintent/Crime.java
@@ -0,0 +1,27 @@
+package com.example.zes.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
